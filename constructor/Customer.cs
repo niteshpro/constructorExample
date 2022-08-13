1cae33d22669c9d8011046637df91ce9ea5a225a
Nitesh Sharma <niteshsharma9199@gmail.com>
2022-08-13 20:11:03 +0530

    First commit

---
+++ constructor/Customer.cs
@@ -0,0 +1,33 @@
+﻿namespace construtorFundamental
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+
+
+        public Customer() //empty constructor
+        {
+        }
+
+        public Customer(int id, string name) //constructor with parameter
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        public void Displaydata()
+        {
+
+            Console.WriteLine("without parameter "+Id + Name);
+        }
+        public void Displaydata(bool value)
+        {
+
+            Console.WriteLine("with parameter " +Id + " " + Name +" " + value);
+        }
+
+
+
+
+    }
+}
---
+++ constructor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace construtorFundamental
+{
+    class Program
+    {
+        static void Main()
+        {
+            Customer customerEmpty = new Customer();
+            Customer constructor2Para = new Customer(1," Nitesh");
+
+            customerEmpty.Displaydata();
+
+            constructor2Para.Displaydata(true);
+
+        }
+    }
+}
